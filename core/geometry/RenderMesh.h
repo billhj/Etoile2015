6512b47799da6215c43d29ec39c07b4401525f92
billhj <gabriel.jing.huang@gmail.com>
2015-05-06 11:29:18 +0200

    add

---
+++ core/geometry/RenderMesh.h
@@ -0,0 +1,135 @@
+/**
+* Copyright(C) 2009-2012                
+* @author Jing HUANG
+* @file RenderMesh.h
+* @brief 
+* @date 1/2/2011
+*/
+
+#pragma once
+#include <string>
+#include <vector>
+#include "math/MathHead.h"
+#include "RenderSubMesh.h"
+#include "Component.h"
+
+#ifdef USING_BOOST
+#include <boost/serialization/serialization.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/serialization/map.hpp>
+#endif
+
+namespace Etoile
+{
+	class RenderMesh
+	{
+	public:
+		RenderMesh(const std::string& name = ""): m_name(name), m_numberOfFaces(0)
+		{
+		}
+		void setDeviceID(long id)
+		{
+			m_deviceID = id;
+		}
+
+		long getDeviceID(){return m_deviceID;}
+
+		RenderMesh(RenderMesh& m)
+		{
+			m_name = m.m_name;
+			m_subRenderMeshList = m.m_subRenderMeshList;
+			m_numberOfFaces = m.m_numberOfFaces;
+
+		}
+		//virtual ~RenderMesh(){}
+		const std::string getName(){return m_name;}
+		void setName(const std::string& name){ m_name = name;}
+
+		virtual void release()
+		{
+			for(unsigned int i = 0; i < m_subRenderMeshList.size(); ++i)
+			{
+				delete m_subRenderMeshList[i];
+			}
+			m_subRenderMeshList.clear();
+		}
+
+		void cleanEmptyRenderSubMesh()
+		{
+			std::vector<RenderSubMesh*>::iterator itor;
+			std::vector<RenderSubMesh*> newlist;
+			for(itor = m_subRenderMeshList.begin(); itor != m_subRenderMeshList.end(); ++itor)
+			{
+				newlist.push_back(*itor);
+			}
+			m_subRenderMeshList.clear();
+			for(itor = newlist.begin(); itor != newlist.end(); ++itor)
+			{
+				RenderSubMesh* submesh = (*itor);
+				if(submesh->getOriginalVertexIndexForFaces().empty())
+				{
+					delete submesh;
+				}else
+				{
+					m_subRenderMeshList.push_back(submesh);
+				}
+			}
+		}
+
+		int getNumberOfFaces() const {return m_numberOfFaces;}
+		unsigned int addRenderSubMesh(RenderSubMesh* sub)
+		{
+			m_subRenderMeshList.push_back(sub);
+			return m_subRenderMeshList.size() - 1;
+		}
+		RenderSubMesh* creatNewRenderSubMesh(const std::string& name = "")
+		{
+			RenderSubMesh* submesh = new RenderSubMesh(name);
+			m_subRenderMeshList.push_back(submesh);
+			return submesh;
+		}
+
+		RenderSubMesh* getRenderSubMesh(int idx)
+		{
+			return m_subRenderMeshList[idx];
+		}
+
+		std::vector<RenderSubMesh*>& getRenderSubMeshList(){return m_subRenderMeshList;}
+
+		void computeAABB()
+		{
+			std::vector<AxisAlignedBoundingBoxf*> aabbs;
+			for(unsigned int i = 0; i < m_subRenderMeshList.size(); ++i)
+			{
+				m_subRenderMeshList[i]->computeAABB();
+				aabbs.push_back(m_subRenderMeshList[i]->getAABB());
+			}
+			m_aabb.build(aabbs);
+		}
+
+		
+
+
+#ifdef USING_BOOST
+		friend class boost::serialization::access;
+
+		template<class Archive>
+		void serialize(Archive & ar, const unsigned int version)
+		{
+			ar & m_name;
+			ar & m_numberOfFaces;
+			ar & m_subRenderMeshList;
+		}
+#endif
+
+		int m_numberOfFaces;
+		std::vector<RenderSubMesh*> m_subRenderMeshList;
+		std::string m_name;
+		long m_deviceID;
+		AxisAlignedBoundingBoxf m_aabb;
+	};
+
+}
---
+++ core/geometry/RenderSubMesh.h
@@ -0,0 +1,127 @@
+﻿/**
+* Copyright(C) 2009-2012                
+* @author Jing HUANG
+* @file RenderSubMesh.h
+* @brief 
+* @date 1/2/2011
+*/
+
+#pragma once
+#include <string>
+#include <vector>
+#include "math/MathHead.h"
+#include "Material.h"
+#include "AxisAlignedBoundingBox.h"
+
+#ifdef USING_BOOST
+#include <boost/serialization/map.hpp>
+#endif
+
+namespace Etoile
+{
+
+	class RenderSubMesh
+	{
+	public:
+		RenderSubMesh(const std::string& name = ""): m_name(name), m_nbVerticesPerFace(3)
+		{
+			p_material = NULL;
+		}
+
+		RenderSubMesh(RenderSubMesh& sub): m_nbVerticesPerFace(3)
+		{
+			m_name = sub.m_name;
+			p_material = sub.p_material;
+			m_nbVerticesPerFace = sub.m_nbVerticesPerFace;
+			m_numberOfFaces = sub.m_numberOfFaces;
+			m_vertices_index_face = sub.m_vertices_index_face;
+			m_normal_index_face = sub.m_normal_index_face;
+			m_texcoord_index_face = sub.m_texcoord_index_face;
+			m_vdata = sub.m_vdata;
+			m_ndata = sub.m_ndata;
+			m_tdata = sub.m_tdata;
+		}
+
+		virtual void release()
+		{
+			m_vertices_index_face.clear();
+			m_normal_index_face.clear();
+			m_texcoord_index_face.clear();
+			m_vdata.clear();
+			m_ndata.clear();
+			m_tdata.clear();
+		}
+
+		const std::string getName(){return m_name;}
+		void setName(const std::string& name){ m_name = name;}
+
+		void setMaterial(Material* material)
+		{
+			p_material = material;
+		}
+
+		Material* getMaterial()
+		{
+			return p_material;
+		}
+
+		void addVertexIndex(int vertices_index)
+		{
+			m_vertices_index_face.push_back(vertices_index);
+		}
+
+		void addNormalIndex(int normal_index)
+		{
+			m_normal_index_face.push_back(normal_index);
+		}
+
+		void addTextureCoordinateIndex(int texcoord_index)
+		{
+			m_texcoord_index_face.push_back(texcoord_index);
+		}
+
+		std::vector<int>& getOriginalVertexIndexForFaces(){return m_vertices_index_face;}
+		std::vector<int>& getOriginalNormalIndexForFaces(){return m_normal_index_face;}
+		std::vector<int>&  getOriginalTextureIndexForFaces(){return m_texcoord_index_face;}
+		std::vector<Vec3f>&  getOriginalVertices(){return m_vdata;} 
+		std::vector<Vec3f>&  getOriginalNormals(){return m_ndata;}
+		std::vector<Vec2f>& getOriginalTextureCoords(){return m_tdata;}
+	
+		void computeAABB()
+		{
+			m_aabb.build(m_vdata);
+		}
+
+		AxisAlignedBoundingBoxf* getAABB(){return &m_aabb; }
+
+#ifdef USING_BOOST
+		friend class boost::serialization::access;
+
+		template<class Archive>
+		void serialize(Archive & ar, const unsigned int version)
+		{
+			ar & m_name;
+			ar & _material;
+			ar & m_nbVerticesPerFace;
+			ar & m_numberOfFaces;
+			ar & m_vdata;
+			ar & m_ndata;
+			ar & m_tdata;
+			ar & m_vertices_index_face;
+		}
+#endif
+	private:
+		Material* p_material;
+	public:
+		int m_nbVerticesPerFace;
+		int m_numberOfFaces;
+	private:
+		std::vector<int> m_vertices_index_face;
+		std::vector<int> m_normal_index_face;
+		std::vector<int> m_texcoord_index_face;
+		std::vector<Vec3f> m_vdata, m_ndata;
+		std::vector<Vec2f> m_tdata;
+		std::string m_name;
+		AxisAlignedBoundingBoxf m_aabb;
+	};
+}
