a77d05f7815d61aa77c5fea95f45fc0f47230951
billhj <gabriel.jing.huang@gmail.com>
2015-05-24 21:34:30 +0200

    try viewtree

--- auxiliary/QGLRenderer/QTSceneTreeWidget.cpp
+++ auxiliary/QGLRenderer/QTSceneTreeWidget.cpp
@@ -1,12 +1,52 @@
 #include "QTSceneTreeWidget.h"
+#include <QStandardItemModel>
+namespace Etoile
+{
+QList<QStandardItem *> prepareRow(const QString &first, const QString &second, const QString &third)
+{
+    QList<QStandardItem *> rowItems;
+    rowItems << new QStandardItem(first);
+    rowItems << new QStandardItem(second);
+    rowItems << new QStandardItem(third);
+    return rowItems;
+}
 
-QTSceneTreeWidget::QTSceneTreeWidget(QWidget *parent)
-	: QWidget(parent)
+QTSceneTreeWidget::QTSceneTreeWidget(QWidget *parent) 
+	: QWidget(parent), p_scene(NULL)
 {
 	ui.setupUi(this);
+	QStandardItemModel* standardModel = new QStandardItemModel ;
+
+    QList<QStandardItem *> preparedRow =prepareRow("first", "second", "third");
+    QStandardItem *item = standardModel->invisibleRootItem();
+    // adding a row to the invisible root item produces a root element
+    item->appendRow(preparedRow);
+
+    QList<QStandardItem *> secondRow =prepareRow("111", "222", "333");
+    // adding a row to an item starts a subtree
+    preparedRow.first()->appendRow(secondRow);
+
+	ui.treeView->setModel(standardModel);
+    ui.treeView->expandAll();
 }
+
 
 QTSceneTreeWidget::~QTSceneTreeWidget()
 {
 
+}
+
+void QTSceneTreeWidget::updateModel()
+{
+	if(p_scene == NULL) return;
+	for(unsigned int i = 0 ; i < p_scene->getEntities().size(); ++i)
+	{
+		Entity* entity = p_scene->getEntities()[i];
+	}
+}
+
+void QTSceneTreeWidget::setScene(Scene* scene)
+{
+	p_scene = scene;
+}
 }
--- auxiliary/QGLRenderer/QTSceneTreeWidget.h
+++ auxiliary/QGLRenderer/QTSceneTreeWidget.h
@@ -2,7 +2,10 @@
 #define SCENETREEWIDGET_H
 
 #include "ui_QTSceneTreeWidget.h"
+#include "geometry/Scene.h"
 
+namespace Etoile
+{
 class QTSceneTreeWidget : public QWidget
 {
 	Q_OBJECT
@@ -10,9 +13,12 @@ class QTSceneTreeWidget : public QWidget
 public:
 	QTSceneTreeWidget(QWidget *parent = 0);
 	~QTSceneTreeWidget();
-
+	void setScene(Scene* scene);
+	void updateModel();
 private:
 	Ui::QTSceneTreeWidget ui;
+	Scene* p_scene;
 };
+}
 
 #endif // SCENETREEWIDGET_H
