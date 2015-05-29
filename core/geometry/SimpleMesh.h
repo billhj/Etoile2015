e6be9001ffa3930e43f578d99efb1410c7969e7a
jhuang <jhuang@telecom-paristech.fr>
2015-05-29 17:42:25 +0200

    add mesh geo

---
+++ core/geometry/SimpleMesh.h
@@ -0,0 +1,54 @@
+/**
+* Copyright(C) 2009-2012                
+* @author Jing HUANG
+* @file SimpleMesh.h
+* @brief 
+* @date 1/2/2011
+*/
+
+#pragma once
+#include <string>
+#include <vector>
+#include "math/MathHead.h"
+
+namespace Etoile
+{
+
+	typedef Vec3f VertexPosition;
+	typedef Vec3f VertexNormal;
+	typedef Vec3f TextureCoordinate;
+	typedef Vec4f ColorRGBA;
+
+	struct SimpleMesh
+	{
+		struct Vertex
+		{
+			int m_posIndex;
+			int m_normalIndex;
+			int m_texcoordIndex;
+		};
+
+		struct Face
+		{
+			std::vector<Vertex> m_verticesInfo;
+		};
+	
+		struct Material
+		{
+			ColorRGBA m_diffuse;
+			ColorRGBA m_specular;
+			ColorRGBA m_ambient;
+			float m_shininess;
+			std::string m_diffuseTextureFilePath;
+			std::string m_specularTextureFilePath;
+			std::string m_ambientTextureFilePath;
+			std::string m_displacementTextureFilePath;
+		};
+
+
+		std::vector<VertexPosition> m_positions;
+		std::vector<VertexNormal> m_normals;
+		std::vector<TextureCoordinate> m_texcoords;
+		std::vector<Face> m_faces;
+	};
+}
