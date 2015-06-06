13a27a004ffcffc806327b35a8ef244e0718c534
billhj <gabriel.jing.huang@gmail.com>
2015-06-06 18:08:45 +0200

    manager resource files

--- applications/SkeletonViewer/main.cpp
+++ applications/SkeletonViewer/main.cpp
@@ -5,10 +5,10 @@
 #include "animation/IK/JacobianTransposeSolver.h"
 int main(int argc, char *argv[])
 {
-	Etoile::IKChain chain("");
-	chain.loadFromFile("C:/Users/Jing/Documents/ikchain.ik");
-	JacobianPseudoInverseSolver solver;
-	solver.compute(&chain, Eigen::Vector3f(1,0,0), true);
+	//Etoile::IKChain chain("");
+	//chain.loadFromFile("C:/Users/Jing/Documents/ikchain.ik");
+	//JacobianPseudoInverseSolver solver;
+	//solver.compute(&chain, Eigen::Vector3f(1,0,0), true);
 
 	QApplication a(argc, argv);
 	QFile stylesheet("./styles/stylesheet.txt");
