12beae4bca48d4b8af85f625789061ec2ec4ced1
billhj <gabriel.jing.huang@gmail.com>
2015-07-26 22:07:12 +0200

    using more

--- auxiliary/animation/IK/IKChain.h
+++ auxiliary/animation/IK/IKChain.h
@@ -57,23 +57,43 @@ namespace Etoile
 		}
 
 		void reset();
+
+		void updateDim(int idx)
+		{
+			Dim* dim = m_dims[idx];
+			if(dim->m_lastIdx >= 0)
+			{
+				m_dim_globalPositions[dim->m_idx] = m_dim_globalPositions[dim->m_lastIdx] + m_dim_globalOrientations[dim->m_lastIdx] * m_dim_localTranslations[dim->m_idx];
+				m_dim_globalOrientations[dim->m_idx] = m_dim_globalOrientations[dim->m_lastIdx] * m_dim_localRotations[dim->m_idx];	
+			}
+			else
+			{
+				m_dim_globalPositions[dim->m_idx] = m_dim_localTranslations[dim->m_idx];
+				m_dim_globalOrientations[dim->m_idx] = m_dim_localRotations[dim->m_idx];
+			}
+		}
+
+		void updateAllDims()
+		{
+			for(unsigned int i = 0; i < m_dims.size(); ++i)
+			{
+				updateJoint(i);
+			}
+		}
+
 		void updateJoint(int idx)
 		{
 			Joint* current = m_joints[idx];
+			Matrix3_ temp = Matrix3_::Identity();
 			for(unsigned int i = 0; i < current->m_dims.size(); ++i)
 			{
 				Dim& dim = current->m_dims[i];
-				if(dim.m_lastIdx >= 0)
-				{
-					m_globalPositions[dim.m_idx] = m_globalPositions[dim.m_lastIdx] + m_globalOrientations[dim.m_lastIdx] * m_localTranslations[dim.m_idx];
-					m_globalOrientations[dim.m_idx] = m_globalOrientations[dim.m_lastIdx] * m_localRotations[dim.m_idx];	
-				}
-				else
-				{
-					m_globalPositions[dim.m_idx] = m_localTranslations[dim.m_idx];
-					m_globalOrientations[dim.m_idx] = m_localRotations[dim.m_idx];
-				}
+				temp = temp * m_dim_localRotations[dim.m_idx];	
 			}
+			m_joint_localRotations[idx] = temp;
+			int last = current->m_dof - 1;
+			m_joint_globalOrientations[idx] = m_dim_globalOrientations[current->m_dims[last].m_idx];
+			m_joint_globalPositions[idx] = m_dim_globalPositions[current->m_dims[last].m_idx];
 		}
 
 		void update()
@@ -88,34 +108,40 @@ namespace Etoile
 		{
 			std::ofstream out;
 			out.open(name);
-			for(unsigned int j = 0; j < m_values.size(); ++j)
+			for(unsigned int j = 0; j < m_dim_values.size(); ++j)
 			{
-				out<<m_values[j]<<", ";
+				out<<m_dim_values[j]<<", ";
 			}
 			out<<std::endl;
-			for(unsigned int j = 0; j < m_anglelimites.size(); ++j)
+			for(unsigned int j = 0; j < m_dim_anglelimites.size(); ++j)
 			{
-				out<<m_anglelimites[j][0]<<", ";
+				out<<m_dim_anglelimites[j][0]<<", ";
 			}
 			out<<std::endl;
-			for(unsigned int j = 0; j < m_anglelimites.size(); ++j)
+			for(unsigned int j = 0; j < m_dim_anglelimites.size(); ++j)
 			{
-				out<<m_anglelimites[j][1]<<", ";
+				out<<m_dim_anglelimites[j][1]<<", ";
 			}
 			out<<std::endl;
 			out.close();
 		}
 
 		std::vector<Joint*> m_joints;
-		std::vector<Matrix3_, Eigen::aligned_allocator<Matrix3_> > m_localRotations;
-		std::vector<Matrix3_, Eigen::aligned_allocator<Matrix3_> > m_globalOrientations;
-		std::vector<Vector3_, Eigen::aligned_allocator<Vector3_> > m_localTranslations;
-		std::vector<Vector3_, Eigen::aligned_allocator<Vector3_> > m_globalPositions;
-		std::string m_name;
+		std::vector<Matrix3_, Eigen::aligned_allocator<Matrix3_> > m_joint_localRotations;
+		std::vector<Matrix3_, Eigen::aligned_allocator<Matrix3_> > m_joint_globalOrientations;
+		std::vector<Vector3_, Eigen::aligned_allocator<Vector3_> > m_joint_localTranslations;
+		std::vector<Vector3_, Eigen::aligned_allocator<Vector3_> > m_joint_globalPositions;
 
-		std::vector<Vector3_, Eigen::aligned_allocator<Vector3_> > m_axis;
-		std::vector<Vector2_, Eigen::aligned_allocator<Vector2_> > m_anglelimites;
-		std::vector<double> m_values;
+		std::vector<Dim*> m_dims;
+		std::vector<Matrix3_, Eigen::aligned_allocator<Matrix3_> > m_dim_localRotations;
+		std::vector<Matrix3_, Eigen::aligned_allocator<Matrix3_> > m_dim_globalOrientations;
+		std::vector<Vector3_, Eigen::aligned_allocator<Vector3_> > m_dim_localTranslations;
+		std::vector<Vector3_, Eigen::aligned_allocator<Vector3_> > m_dim_globalPositions;
+		std::vector<double> m_dim_values;
+		std::vector<Vector3_, Eigen::aligned_allocator<Vector3_> > m_dim_axis;
+		std::vector<Vector2_, Eigen::aligned_allocator<Vector2_> > m_dim_anglelimites;
+		std::string m_name;
+		
 		std::vector<double> m_drLimits_positive;
 		std::vector<double> m_drLimits_negative;
 
