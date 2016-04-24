e26885e17b8eb66dda05203ebaec81f88936de90
billhj <gabriel.jing.huang@gmail.com>
2016-04-24 22:54:21 +0200

    add

--- applications/BVHApp/main.cpp
+++ applications/BVHApp/main.cpp
@@ -5,6 +5,10 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	QGLFormat glf = QGLFormat::defaultFormat();
+    glf.setSampleBuffers(true);
+    glf.setSamples(16);
+    QGLFormat::setDefaultFormat(glf);
 	BVHApp w;
 	w.show();
 	return a.exec();
